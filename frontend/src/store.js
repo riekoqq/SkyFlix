38dfb182a5012e6f3947106c5e37606a8a6e919a
Justin Rieko Tolentino <jstolentino2@student.hau.edu.ph>
2025-03-27 03:35:11 +0800

    more reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import { movieDetailsReducer } from "./reducers/movieReducers";
 import { userLoginReducer, userProfileReducer, userRegisterReducer } from "./reducers/userReducers";
 import { watchHistoryReducer } from "./reducers/watchHistoryReducers";
 import { bookmarkReducer } from "./reducers/bookmarkReducers";
+import { subscriptionReducer } from "./reducers/subscriptionReducers";
 
 const reducer = combineReducers({
     movieTopPicks: movieTopPicksReducer,
@@ -16,15 +17,22 @@ const reducer = combineReducers({
     searchMovies: searchMoviesReducer,
     watchHistory: watchHistoryReducer,
     movieWatchHistory: movieWatchHistoryReducer,
-    bookmarks: bookmarkReducer
+    bookmarks: bookmarkReducer,
+    subscription: subscriptionReducer,
+
 })
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null;
 
+const paymentMethodFromStorage = localStorage.getItem("paymentMethod")
+    ? JSON.parse(localStorage.getItem("paymentMethod"))
+    : '';
+
 const initialState = {
     userLogin: { userInfo: userInfoFromStorage },
+    subscription: { paymentMethod: paymentMethodFromStorage }
 };
 
 const store = configureStore({
