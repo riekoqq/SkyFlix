e2388bc2841da0ab654ebcbd9928a214d7d0b488
Justin Rieko Tolentino <jstolentino2@student.hau.edu.ph>
2025-03-28 14:04:20 +0800

    eh

--- backend/Backend/settings.py
+++ backend/Backend/settings.py
@@ -56,7 +56,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://skyflix-frontend.vercel.app',  # Add your frontend URL here
+]
+
 
 ROOT_URLCONF = 'Backend.urls'
 
